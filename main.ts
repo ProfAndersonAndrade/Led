e38f642abc117073c341d8c7bc31a47428e44851
ProfAndersonAndrade <160557397+ProfAndersonAndrade@users.noreply.github.com>
2024-03-12 20:31:28 -0300

    Primeira alteração

--- main.ts
+++ main.ts
@@ -1,3 +1,36 @@
+input.onButtonPressed(Button.A, function () {
+    Aleatorio()
+    basic.pause(5000)
+})
+function Aleatorio () {
+    pins.analogWritePin(AnalogPin.P0, randint(0, 1023))
+    pins.analogWritePin(AnalogPin.P1, randint(0, 1023))
+    pins.analogWritePin(AnalogPin.P2, randint(0, 1023))
+    basic.showString("RANDOM")
+}
+function Green () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    pins.analogWritePin(AnalogPin.P2, 0)
+    basic.showString("GREEN")
+}
+function Blue () {
+    pins.analogWritePin(AnalogPin.P0, 0)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 1023)
+    basic.showString("BLUE")
+}
+function Red () {
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    pins.analogWritePin(AnalogPin.P1, 0)
+    pins.analogWritePin(AnalogPin.P2, 0)
+    basic.showString("RED")
+}
 basic.forever(function () {
-	
+    Red()
+    basic.pause(5000)
+    Green()
+    basic.pause(5000)
+    Blue()
+    basic.pause(5000)
 })
